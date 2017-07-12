50bc670649cf414e20671cc9c3bffba18742eff7
Jean-Paul Lacy <lacyjpr@gmail.com>
2017-07-12 10:03:28 -0700

    Fix tests broken by redux

--- app/store/configureStore.jsx
+++ app/store/configureStore.jsx
@@ -1,16 +1,18 @@
 var redux = require('redux');
 var {searchTextReducer, showCompletedReducer, todosReducer} = require('reducers');
 
-export var configure = () => {
+export var configure = (initalState = {}) => {
 	var reducer = redux.combineReducers({
 		searchText: searchTextReducer,
 		showCompleted: showCompletedReducer,
 		todos: todosReducer
 	});
 
-	var store = redux.createStore(reducer, redux.compose(
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	));
+	// var store = redux.createStore(reducer, redux.compose(
+	// 	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	// ));
+	var composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || redux.compose;
+	var store = redux.createStore(reducer, initalState, composeEnhancers());
 
 	return store;
 };
