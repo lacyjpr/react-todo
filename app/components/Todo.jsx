dbe503724686437c499d8f6bbcfe1f7b5595a91f
Jean-Paul Lacy <lacyjpr@gmail.com>
2017-07-15 10:45:49 -0700

    Add actions for firebase login and logout

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -6,7 +6,7 @@ var actions = require('actions');
 export var Todo = React.createClass({
 	render: function() {
 		var {id, text, completed, createdAt, completedAt, dispatch} = this.props;
-		var todoClassName = completed ? 'todo todo-completed' : 'todo'
+		var todoClassName = completed ? 'todo todo-completed' : 'todo';
 		var renderDate = () => {
 			var message = 'Created ';
 			var timestamp = createdAt;
@@ -16,7 +16,7 @@ export var Todo = React.createClass({
 				timestamp = completedAt;
 			}
 
-			return message + moment.unix(timestamp).format('MMM Do YYYY @ h:mm a')
+			return message + moment.unix(timestamp).format('MMM Do YYYY @ h:mm a');
 		};
 
 		return (
